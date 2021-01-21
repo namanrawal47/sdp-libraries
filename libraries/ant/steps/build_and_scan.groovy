a03a1acaac7ae65c7b3f80e1a3e8842dd41477d0
namanrawal47 <namanrawal360@gmail.com>
2021-01-21 15:34:07 -0500

    moved files into steps folder

---
+++ libraries/ant/steps/build_and_scan.groovy
@@ -0,0 +1,47 @@
+void call(){
+    stage("Ant: Build and Scan"){
+        //navigate into subdirectory
+        dir(config.source_dir){
+          //define command string
+          ant_command = "ant"
+
+          if(config.build_command) {
+            ant_command += " " + config.build_command
+          }  else { error("missing build command") }
+          if(config.build_file) {
+            ant_command += " -f " + config.build_file
+          } else { error("missing build file") }
+          
+          // check for SonarQube flags  
+          if(config.sonar.flags && config.sonar.enabled==true) {
+            //def sonar=config.sonar
+            ant_command += " sonar " + config.sonar.flags
+              if(config.sonar.properties_file){
+                ant_command += " " + config.sonar.properties_file
+            }
+              if(config.sonar.creds) {
+                  withCredentials([usernamePassword(credentialsId: config.sonar.creds, passwordVariable: 'sonar_pw', usernameVariable: 'sonar_user')]) {
+                    ant_command += " -Dsonar.login=${sonar_user} -Dsonar.password=${sonar_pw}" 
+              }
+            }
+              if(config.sonar.java_binaries){
+                  ant_command += " -Dsonar.java.binaries=\"" + config.sonar.java_binaries + "\""
+            }
+              if(config.sonar.exclusions){
+                  ant_command += " -Dsonar.exclusions=\"" + config.sonar.exclusions + "\""
+            }
+            // increase timeout to 5 minutes
+            def sonar_timeout = config.sonar.timeout ?: 300
+            ant_command += " -Dsonar.ws.timeout=${sonar_timeout}"
+            //ant_command += " -Dsonar.ws.timeout=${300}"
+          } else { error "You must provide sonar flags for step build_and_scan()" }
+
+          echo "current ant command: " + ant_command
+          
+          //check Jenkins agent type and run command
+          if(pipelineConfig.agent_type == "windows") {
+            bat ant_command
+          } else { sh ant_command }
+        } // end dir block
+    } // end stage block
+}
