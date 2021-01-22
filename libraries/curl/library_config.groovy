d1fdd07b783b9b49ac74239a44233ed19c90f139
namanrawal47 <namanrawal360@gmail.com>
2021-01-22 13:54:00 -0500

    created library_config for curl library

---
+++ libraries/curl/library_config.groovy
@@ -0,0 +1,11 @@
+fields{
+    optional{
+        
+    }
+    required{
+        directory = String
+        // probably need something for artifact repo credentials
+        file = String
+        url = String
+    }
+}
